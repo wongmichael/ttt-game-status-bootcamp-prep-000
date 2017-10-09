adb54a281b2fcde8c1df11857e08c496885defd2
Michael Wong <mik9080@gmail.com>
2017-10-09 16:29:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,11 +25,10 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
+  }
     if (position_1 == "X" && position_2 == "X" && position_3 =="X") || (position_1 == "O" && position_2 == "O" && position_3 =="O")
       return win_combination
     #else
     #  false
     end
-  }
 end
